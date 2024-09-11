4b40a08dd508748a8010185eb6dcc049fddb03da
Cauã Tavares <tavarescauac@gmail.com>
2024-09-11 12:01:23 -0300

    ci: implementation of TankControl Layout Page and Routing on Card Click (#6)
    * refac: login screen to new layout
    
    * ci: grape reception screen
    
    * fix: unique id in renderItem and pressure strange behavior
    
    * ci: correctly naming tankControl Page and initiallizing implementation of a specific tank info

--- app/index.tsx
+++ app/index.tsx
@@ -5,29 +5,47 @@ import { Image, Text, View } from "react-native";
 
 export default function Login() {
   return (
-    <SafeAreaView className="flex-1 justify-between">
-      <View>
-        <Image
-          source={require("@/assets/images/logo.png")}
-          style={{ marginLeft: -40 }}
-        />
-        <Text className="text-4xl text-black mt-4 mb-8">
-          Desfrute da <B>inovação</B> que a <B>Scan.AI</B> pode oferecer.
-        </Text>
-        <View className="w-full mb-24 gap-2">
-          <InputBox title={"Nome de usuário"} placeholder={"carlos_andrade"} />
-          <InputBox
-            title={"Senha"}
-            placeholder={"************"}
-            secureTextEntry={true}
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 justify-between">
+        <View>
+          <Image
+            source={require("@/assets/images/logo.png")}
+            style={{ marginLeft: -40 }}
           />
+<<<<<<< HEAD
+          <Text className="text-4xl text-black mt-4 mb-8">
+            Desfrute da <B>inovação</B> que a <B>Scan.AI</B> pode oferecer.
+          </Text>
+          <View className="w-full mb-6 mt-20 gap-2">
+            <InputBox title="Nome de usuário" placeholder="carlos_andrade" />
+            <InputBox
+              title="Senha"
+              placeholder="************"
+              secureTextEntry={true}
+            />
+          </View>
+          <View className="flex w-full items-center mt-16">
+            <Button placeholder="Acessar" route="/(tankControl)" />
+            <Text className="mt-4 text-[#9B9B9B] text-[10px]">
+              © 2024 Scan.AI. Todos os direitos reservados.
+            </Text>
+            <Text className="text-xs text-[#9B9B9B]">
+              scanaistartup@gmail.com
+            </Text>
+          </View>
+=======
         </View>
         <View className="flex w-full justify-between">
           <Button placeholder="Acessar" route="/home" />
           <Button placeholder="Cadastrar novo usuário" route="/signup" />
+>>>>>>> 2ccfb992c984228666f9b54e947a9b5d41418ef8
         </View>
       </View>
-      <Footer />
+      <View className="items-center mt-auto mb-4">
+        <Text className="text-xs text-[#9B9B9B]">
+          Política de Privacidade Termos de Uso
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -35,19 +53,3 @@ export default function Login() {
 const B = (props: any) => (
   <Text style={{ fontWeight: "bold" }}>{props.children}</Text>
 );
-
-function Footer() {
-  return (
-    <View className="flex items-center">
-      <View className="flex w-full items-center justify-center">
-        <Text className="mt-4 text-[#9B9B9B] text-[10px]">
-          © 2024 Scan.AI. Todos os direitos reservados.
-        </Text>
-        <Text className="text-xs text-[#9B9B9B]">scanaistartup@gmail.com</Text>
-      </View>
-      <Text className="flex text-xs mt-4 items-center justify-center text-[#9B9B9B]">
-        Política de Privacidade Termos de Uso
-      </Text>
-    </View>
-  );
-}
