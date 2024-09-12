0d8c4ac9e8ce06f41cae4518cbc823bc1bd5d0c6
cauatn <tavarescauac@gmail.com>
2024-09-12 19:15:26 -0300

    ci: card for a non empty tank

--- app/(tankControl)/tank/[tank].tsx
+++ app/(tankControl)/tank/[tank].tsx
@@ -1,7 +1,14 @@
 import ActivityCard from "@/components/ActivityCard";
 import AppHeader from "@/components/AppHeader";
 import { useLocalSearchParams } from "expo-router";
-import { Boxes, Cylinder, Grape } from "lucide-react-native";
+import {
+  ArrowDown,
+  Boxes,
+  ChevronDown,
+  Cylinder,
+  Grape,
+  Search,
+} from "lucide-react-native";
 import {
   View,
   Text,
@@ -76,6 +83,7 @@ export default function Tank() {
         <Text className="text-2xl">Remessas associadas</Text>
         <Card />
       </View>
+      <CardNotEmpty />
     </SafeAreaView>
   );
 }
@@ -87,6 +95,46 @@ const Card = () => {
       <View style={styles.row}>
         <Text style={styles.shipment}>101 - 2º Talão</Text>
         <Text style={styles.details}>Detalhes</Text>
+      </View>
+    </View>
+  );
+};
+
+const CardNotEmpty = () => {
+  return (
+    <View>
+      <Text className="text-2xl font-medium">Resumo da última análise</Text>
+      <View className="bg-white" style={styles.card}>
+        <Text className="text-xl text-black font-medium">09/08/24</Text>
+        <View className="flex-row justify-between">
+          <Text className="text-gray-500">Densidade</Text>
+          <View style={styles.bottomAligned}>
+            <Text className="text-xl">996</Text>
+            <Text className="text-gray-500">g/cm3</Text>
+          </View>
+        </View>
+        <View className="flex-row justify-between">
+          <Text className="text-gray-500">Temperatura</Text>
+          <View style={styles.bottomAligned}>
+            <ChevronDown size="25px" color="red" />
+            <Text className="text-xl">20</Text>
+            <Text className="text-gray-500">ºC</Text>
+          </View>
+        </View>
+        <View className="flex-row justify-between">
+          <Text className="text-gray-500">Pressão</Text>
+          <View style={styles.bottomAligned}>
+            <ChevronDown
+              size="25px"
+              color="green"
+              style={{
+                transform: [{ rotate: "180deg" }],
+              }}
+            />
+            <Text className="text-xl">5.7</Text>
+            <Text className="text-gray-500">bar</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -119,5 +167,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "semibold",
     color: "blue",
+  },
+  bottomAligned: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    gap: 4, // Use this if gap is not supported, otherwise you can remove it
   },
 });
