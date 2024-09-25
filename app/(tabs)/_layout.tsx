08a6c210295020b352659e663ae0ae39602de64e
jvictordev1 <josevictorcruzrb@gmail.com>
2024-09-25 20:57:01 -0300

    style: custom navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import TabBar from "@/components/TabBar";
 import { Tabs } from "expo-router";
 import { Cylinder, House } from "lucide-react-native";
 import React from "react";
@@ -8,23 +9,26 @@ export default function TabLayout() {
   };
   return (
     <Tabs
-      screenOptions={{ tabBarActiveTintColor: "#171717", headerShown: false }}
+      screenOptions={{
+        tabBarActiveTintColor: "#171717",
+        tabBarInactiveTintColor: "#C0C0C0",
+        headerShown: false,
+      }}
+      tabBar={(props) => <TabBar {...props} />}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Início",
-          tabBarIcon: ({ color }) => (
-            <House size={iconProps.size} color={color} />
-          ),
+          tabBarIcon: ({ size, color }) => <House size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="tankControl"
         options={{
           title: "Tanques",
-          tabBarIcon: ({ color }) => (
-            <Cylinder size={iconProps.size} color={color} />
+          tabBarIcon: ({ size, color }) => (
+            <Cylinder size={size} color={color} />
           ),
         }}
       />
