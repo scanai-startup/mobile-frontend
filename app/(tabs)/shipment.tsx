3a3ebe203232c450580aaa434f453aafa05470db
jvictordev1 <josevictorcruzrb@gmail.com>
2024-10-02 09:14:53 -0300

    refac: creating shipmentCard component on shipment page

--- app/(tabs)/shipment.tsx
+++ app/(tabs)/shipment.tsx
@@ -86,62 +86,11 @@ export default function Shipment() {
               icon={<CirclePlus color="white" />}
             />
           </Link>
-
           <FlatList
             data={data}
             keyExtractor={(item) => String(item.id)}
             showsVerticalScrollIndicator={false}
-            renderItem={({ item }) => (
-              <View className="bg-white rounded-xl border border-neutral-250">
-                <View className="flex-row justify-between items-center p-5">
-                  <View className="flex-row gap-1 items-center">
-                    <Text
-                      className="text-2xl font-semibold"
-                      style={{ color: "#171717" }}
-                    >
-                      {item.number}
-                    </Text>
-                    <Text className="text-base text-neutral-400">
-                      {item.ticket}° Talão
-                    </Text>
-                  </View>
-                  <TouchableOpacity>
-                    <Text className="text-blue-500">Detalhes</Text>
-                  </TouchableOpacity>
-                </View>
-                <View className="flex-1 border-b border-neutral-250"></View>
-                <View className="flex-col p-5">
-                  <View className="flex-row justify-between items-center">
-                    <Text className="text-lg text-neutral-900">
-                      Identificador
-                    </Text>
-                    <Text className="text-xl font-semibold text-neutral-900">
-                      {item.id}
-                    </Text>
-                  </View>
-                  <View className="flex-row justify-between items-center">
-                    <Text className="text-lg text-neutral-900">
-                      Data recebido
-                    </Text>
-                    <Text className="text-xl font-semibold text-neutral-900">
-                      {item.date}
-                    </Text>
-                  </View>
-                  <View className="flex-row justify-between items-center">
-                    <Text className="text-lg text-neutral-900">Casta</Text>
-                    <Text className="text-xl font-semibold text-neutral-900">
-                      {item.casta}
-                    </Text>
-                  </View>
-                  <View className="flex-row justify-between items-center">
-                    <Text className="text-lg text-neutral-900">Tipo</Text>
-                    <Text className="text-xl font-semibold text-neutral-900">
-                      {item.type}
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            )}
+            renderItem={({ item }) => <ShipmentCard {...item} />}
             contentContainerStyle={{
               paddingVertical: 20,
               paddingBottom: 160,
@@ -151,5 +100,51 @@ export default function Shipment() {
         </View>
       </SafeAreaView>
     </>
+  );
+}
+export function ShipmentCard(shipment: ShipmentCards) {
+  return (
+    <View className="bg-white rounded-xl border border-neutral-250">
+      <View className="flex-row justify-between items-center p-5">
+        <View className="flex-row gap-1 items-center">
+          <Text className="text-2xl font-semibold" style={{ color: "#171717" }}>
+            {shipment.number}
+          </Text>
+          <Text className="text-base text-neutral-400">
+            {shipment.ticket}° Talão
+          </Text>
+        </View>
+        <TouchableOpacity>
+          <Text className="text-blue-500">Detalhes</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex-1 border-b border-neutral-250"></View>
+      <View className="flex-col p-5">
+        <View className="flex-row justify-between items-center">
+          <Text className="text-lg text-neutral-900">Identificador</Text>
+          <Text className="text-xl font-semibold text-neutral-900">
+            {shipment.id}
+          </Text>
+        </View>
+        <View className="flex-row justify-between items-center">
+          <Text className="text-lg text-neutral-900">Data recebido</Text>
+          <Text className="text-xl font-semibold text-neutral-900">
+            {shipment.date}
+          </Text>
+        </View>
+        <View className="flex-row justify-between items-center">
+          <Text className="text-lg text-neutral-900">Casta</Text>
+          <Text className="text-xl font-semibold text-neutral-900">
+            {shipment.casta}
+          </Text>
+        </View>
+        <View className="flex-row justify-between items-center">
+          <Text className="text-lg text-neutral-900">Tipo</Text>
+          <Text className="text-xl font-semibold text-neutral-900">
+            {shipment.type}
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 }
