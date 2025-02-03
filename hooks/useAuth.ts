ab931fb4b3ab737a17a5ceef19fad785dedc4841
Cauã Tavares <tavarescauac@gmail.com>
2025-02-03 15:26:24 -0300

    fix (login): useAuth Login correct redirecting

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -2,32 +2,35 @@ import { useTokenStore } from "@/store/userData";
 import { useToast } from "./useToast";
 import apiInstance from "@/api/apiInstance";
 import * as SecureStore from "expo-secure-store";
+import { useRouter } from "expo-router";
 
 export function useAuth() {
-    const { setToken } = useTokenStore();
-    const toast = useToast();
-  
-    const login = async (matricula: string, senha: string) => {
-      try {
-        const response = await apiInstance.post("/auth/login", {
-          matricula,
-          senha,
-        });
-        const token = response.data.token;
-  
-        setToken(token);
-        await SecureStore.setItemAsync("user-token", token);
-  
-        toast({ heading: "Sucesso", message: "Seja bem-vindo!" });
-      } catch (error) {
-        toast({
-          heading: "Erro",
-          message: "Houve um erro, por favor verifique suas credenciais.",
-          type: "error",
-        });
-        console.error(error);
-      }
-    };
-  
-    return { login };
-  }
+  const { setToken } = useTokenStore();
+  const toast = useToast();
+  const router = useRouter();
+
+  const login = async (matricula: string, senha: string) => {
+    try {
+      const response = await apiInstance.post("/auth/login", {
+        matricula,
+        senha,
+      });
+      const token = response.data.token;
+
+      setToken(token);
+      await SecureStore.setItemAsync("user-token", token);
+
+      toast({ heading: "Sucesso", message: "Seja bem-vindo!" });
+      router.push("/(tabs)/");
+    } catch (error) {
+      toast({
+        heading: "Erro",
+        message: "Houve um erro, por favor verifique suas credenciais.",
+        type: "error",
+      });
+      console.error(error);
+    }
+  };
+
+  return { login };
+}
