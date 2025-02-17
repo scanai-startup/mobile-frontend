17e3f5f5a562780d58ccc65ec7fa7ef7916e8995
Cauã Tavares <tavarescauac@gmail.com>
2025-02-17 17:30:14 -0300

    chore (login): changed login default credentials

--- hooks/useAutoLogin.ts
+++ hooks/useAutoLogin.ts
@@ -6,30 +6,30 @@ import apiInstance from "@/api/apiInstance";
 import * as SecureStore from "expo-secure-store";
 
 export function useAutoLogin() {
-    const { setToken } = useTokenStore();
-    const router = useRouter();
-    const toast = useToast();
-  
-    useEffect(() => {
-      const autoLoginIfDevelopment = async () => {
-        if (process.env.EXPO_PUBLIC_ENV === "development") {
-          try {
-            const response = await apiInstance.post("/auth/login", {
-              matricula: "ADMIN",
-              senha: "ADMIN",
-            });
-  
-            const token = response.data.token;
-            setToken(token);
-            await SecureStore.setItemAsync("user-token", token);
-            router.push("/(tabs)/");
-            toast({ heading: "Sucesso", message: "Seja bem-vindo!" });
-          } catch (e) {
-            console.error("Auto-login failed", e);
-          }
+  const { setToken } = useTokenStore();
+  const router = useRouter();
+  const toast = useToast();
+
+  useEffect(() => {
+    const autoLoginIfDevelopment = async () => {
+      if (process.env.EXPO_PUBLIC_ENV === "development") {
+        try {
+          const response = await apiInstance.post("/auth/login", {
+            matricula: "123",
+            senha: "senha123",
+          });
+
+          const token = response.data.token;
+          setToken(token);
+          await SecureStore.setItemAsync("user-token", token);
+          router.push("/(tabs)/");
+          toast({ heading: "Sucesso", message: "Seja bem-vindo!" });
+        } catch (e) {
+          console.error("Auto-login failed", e);
         }
-      };
-  
-      autoLoginIfDevelopment();
-    }, []);
-  }
+      }
+    };
+
+    autoLoginIfDevelopment();
+  }, []);
+}
