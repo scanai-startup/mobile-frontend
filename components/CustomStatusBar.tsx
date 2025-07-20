c8acc7efbcde38acd3c5fe022e5d5cc3dd414ddd
Cauã Tavares <tavarescauac@gmail.com>
2025-07-20 16:37:16 -0300

    fix: fix errors

--- components/CustomStatusBar.tsx
+++ components/CustomStatusBar.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar, StatusBarProps } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 interface CustomStatusBarProps extends StatusBarProps {
